be8760b65042cc0218d5e84966f86aed9ad42ed0
Pratham <prathamlashkari841@gmail.com>
2024-10-06 22:40:17 +0530

    creating dointernal method

--- server/src/main/java/com/bitwatch/config/SecurityFilter.java
+++ server/src/main/java/com/bitwatch/config/SecurityFilter.java
@@ -1,14 +1,44 @@
 package com.bitwatch.config;
 
-import java.net.http.HttpRequest;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class SecurityFilter {
 
-  SecurityFilterChain securityFilterChain(HttpRequest http) throws Exception {
-    return null;
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers("/api/**").authenticated()
+            .anyRequest().permitAll())
+        .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+        .csrf(csrf -> csrf.disable())
+        .cors(cors -> cors.configurationSource(corsConfigurationSource()));
+    return http.build();
+  }
+
+  private CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration cors = new CorsConfiguration();
+    cors.setAllowedHeaders(Collections.singletonList("*"));
+    cors.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173"));
+    cors.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+    cors.addExposedHeader("Authorization");
+    cors.setMaxAge(3600L);
+    cors.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", cors);
+    return source;
   }
 }
