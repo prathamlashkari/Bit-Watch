6d1065c048ceaa867ed83eed861b8858a4441edf
Pratham <prathamlashkari841@gmail.com>
2024-10-06 22:08:06 +0530

    setup enums and twofactorauth

--- server/src/main/java/com/bitwatch/models/UserModel.java
+++ server/src/main/java/com/bitwatch/models/UserModel.java
@@ -1,11 +1,22 @@
 package com.bitwatch.models;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.bitwatch.enums.USER_ROLE;
 
 import lombok.Data;
 
 @Data
 @Document(collection = "User")
 public class UserModel {
+
+  @Id
+  private String id;
+  private String fullName;
+  private String email;
+  private String password;
+  private TwoFactorAuth twoFactorAuth = new TwoFactorAuth();
+  private USER_ROLE role = USER_ROLE.ROLE_CUSTOMER;
 
 }
---
+++ server/src/main/java/com/bitwatch/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.bitwatch.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bitwatch.models.UserModel;
+
+@Repository
+public interface UserRepository extends MongoRepository<UserModel, String> {
+
+}
