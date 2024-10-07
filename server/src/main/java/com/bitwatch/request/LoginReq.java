eb5eda309c85461840d6dcd04fad32fe87ccbf8b
Pratham <prathamlashkari841@gmail.com>
2024-10-07 23:01:10 +0530

    added login req

---
+++ server/src/main/java/com/bitwatch/request/LoginReq.java
@@ -0,0 +1,11 @@
+package com.bitwatch.request;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class LoginReq {
+  private String email;
+  private String passwrod;
+}
