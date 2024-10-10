570896c7adc1499a2db61ed5e1c800e6107ece59
Pratham <prathamlashkari841@gmail.com>
2024-10-10 18:14:40 +0530

    implmenting verification methods

---
+++ server/src/main/java/com/bitwatch/service/impl/VerificationCodeServiceImpl.java
@@ -0,0 +1,51 @@
+package com.bitwatch.service.impl;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bitwatch.enums.VerificationType;
+import com.bitwatch.models.UserModel;
+import com.bitwatch.models.VerificationCode;
+import com.bitwatch.repository.VerificationCodeRepository;
+import com.bitwatch.service.VerificationCodeSerice;
+import com.bitwatch.utils.OtpUtils;
+
+@Service
+public class VerificationCodeServiceImpl implements VerificationCodeSerice {
+
+  @Autowired
+  private VerificationCodeRepository verificationCodeRepository;
+
+  @Override
+  public VerificationCode sendVerificationCode(UserModel userModel, VerificationType verificationType) {
+    VerificationCode verificationCode1 = new VerificationCode();
+    verificationCode1.setOtp(OtpUtils.generateOtp());
+    verificationCode1.setVerificationType(verificationType);
+    verificationCode1.setUserId(userModel.getId());
+    return verificationCodeRepository.save(verificationCode1);
+  }
+
+  @Override
+  public VerificationCode getVerificationCodeById(String id) throws Exception {
+    Optional<VerificationCode> verificationCode = verificationCodeRepository.findById(id);
+    if (verificationCode.isPresent()) {
+      return verificationCode.get();
+    }
+    throw new Exception("Verification Code not Found");
+  }
+
+  @Override
+  public VerificationCode getvVerificationCodeByUserId(String userId) {
+
+    throw new UnsupportedOperationException("Unimplemented method 'getvVerificationCodeByUserId'");
+  }
+
+  @Override
+  public void deleteVerificationCodeById(VerificationCode verificationCode) {
+
+    throw new UnsupportedOperationException("Unimplemented method 'deleteVerificationCodeById'");
+  }
+
+}
