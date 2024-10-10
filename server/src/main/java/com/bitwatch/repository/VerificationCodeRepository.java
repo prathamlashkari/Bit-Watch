28b0429de8c9fcc5334c54640055901e6b04ff77
Pratham <prathamlashkari841@gmail.com>
2024-10-10 17:55:16 +0530

    added methods in repo

---
+++ server/src/main/java/com/bitwatch/repository/VerificationCodeRepository.java
@@ -0,0 +1,12 @@
+package com.bitwatch.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bitwatch.models.VerificationCode;
+
+@Repository
+public interface VerificationCodeRepository extends MongoRepository<VerificationCode, String> {
+
+  public VerificationCode findByUserId(String userId);
+}
