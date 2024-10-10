569155814fe4c09ade74364e2fae5ebb75ea1af8
Pratham <prathamlashkari841@gmail.com>
2024-10-10 17:53:35 +0530

    added verficationCode model and repo

---
+++ server/src/main/java/com/bitwatch/models/VerificationCode.java
@@ -0,0 +1,20 @@
+package com.bitwatch.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.bitwatch.enums.VerificationType;
+
+import lombok.Data;
+
+@Data
+@Document(collection = "VerificationCode")
+public class VerificationCode {
+
+  @Id
+  private String id;
+  private String otp;
+  private String userId;
+  private String mobile;
+  private VerificationType verificationType;
+}
